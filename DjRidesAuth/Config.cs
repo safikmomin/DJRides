5c579e391caf53bdb86d2ddb474660213baaa5fb
Safik Momin <Safik Momin>
2017-12-22 20:31:07 -0600

    Add project files.

---
+++ DjRidesAuth/Config.cs
@@ -0,0 +1,100 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DjRidesAuth
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("MainApi1", "My API")
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "MainApi1" }
+                },
+
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    RequireConsent = false,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "MainApi1"
+                    },
+                    AllowOfflineAccess = true
+                },
+
+                // OpenID Connect hybrid flow and client credentials client (MVC)
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    
+                    AllowedGrantTypes = GrantTypes.Code ,
+
+                    RequireConsent = false,
+                    RequireClientSecret = true,
+                    //RequirePkce = true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    //RedirectUris = { "http://localhost:55994/signin-oidc" },
+                    //PostLogoutRedirectUris = { "http://localhost:55994/signout-callback-oidc" },
+
+                    RedirectUris = { "http://localhost:44375/Account/Login" },
+                    PostLogoutRedirectUris = { "http://localhost:44375/" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "MainApi1"
+                    },
+                    AllowOfflineAccess = true
+                }
+            };
+        }
+    }
+}
---
+++ testingApp/Program.cs
@@ -0,0 +1,112 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testingApp
+{
+    public class Program
+    {
+        static TokenClient _tokenClient;
+
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            Console.Title = "Console ResourceOwner Flow RefreshToken";
+
+            //var disco = await DiscoveryClient.GetAsync(Constants.Authority);
+            //if (disco.IsError) throw new Exception(disco.Error);
+
+            _tokenClient = new TokenClient("https://localhost:44375/connect/token", "client", "secret");
+
+            var response = await RequestTokenAsync();
+            Console.WriteLine(response.Json);
+            //response.Show();
+
+            Console.ReadLine();
+
+            var refresh_token = response.RefreshToken;
+
+            var somecall = CallServiceAsync(response.AccessToken);
+
+            while (true)
+            {
+                response = await RefreshTokenAsync(refresh_token);
+                ShowResponse(response);
+
+                Console.ReadLine();
+                await CallServiceAsync(response.AccessToken);
+
+                if (response.RefreshToken != refresh_token)
+                {
+                    refresh_token = response.RefreshToken;
+                }
+            }
+        }
+
+        static async Task<TokenResponse> RequestTokenAsync()
+        {
+            return await _tokenClient.RequestResourceOwnerPasswordAsync(
+                "safikmomin@hotmail.com",
+                "Sm786110!",
+                "MainApi1 offline_access profile openid");
+        }
+
+        private static async Task<TokenResponse> RefreshTokenAsync(string refreshToken)
+        {
+            Console.WriteLine("Using refresh token: {0}", refreshToken);
+
+            return await _tokenClient.RequestRefreshTokenAsync(refreshToken);
+        }
+
+        static async Task CallServiceAsync(string token)
+        {
+            var baseAddress = "http://localhost:63285";
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = await client.GetStringAsync("/api/values");
+            
+            Console.WriteLine(JArray.Parse(response));
+        }
+
+        private static void ShowResponse(TokenResponse response)
+        {
+            if (!response.IsError)
+            {
+                Console.WriteLine(response.Json);
+
+                if (response.AccessToken.Contains("."))
+                {
+
+                    var parts = response.AccessToken.Split('.');
+                    var header = parts[0];
+                    var claims = parts[1];
+
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(header))));
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(claims))));
+                }
+            }
+            else
+            {
+                if (response.ErrorType == ResponseErrorType.Http)
+                {
+                    Console.WriteLine(response.Error);
+                    Console.WriteLine(response.HttpStatusCode);
+                }
+                else
+                {
+                    Console.WriteLine(response.Json);
+                }
+            }
+        }
+    }
+}
